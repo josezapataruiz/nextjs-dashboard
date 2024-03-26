a178c82cf01f7dc43b096ccbdf95e4720149e871
Alex Zapata <alex@flitware.com>
2024-03-26 15:17:19 -0500

    feat: add authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { revalidatePath } from 'next/cache';
 import { createMySQLPool } from './db-connection';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -122,5 +124,24 @@ export async function deleteInvoice(id: string) {
     return {
       message: `Error de Base de Datos: No se puede eliminar la factura. ${error}`,
     };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
