41ea11c9ba86d1530366f545362c5171a7906e27
Alex Zapata <alex@flitware.com>
2024-03-25 23:09:54 -0500

    feat: add error handler
    feat: add validation in create and edit forms

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,46 +3,124 @@
 import { revalidatePath } from 'next/cache';
 import { createMySQLPool } from './db-connection';
 import { redirect } from 'next/navigation';
+import { z } from 'zod';
 
-export async function createInvoice(formData: FormData) {
-  const amount = Number(formData.get('amount'));
-  const customerId = formData.get('customerId');
-  const status = formData.get('status');
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(preState: State, formData: FormData) {
+  // Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await createMySQLPool.query(
-    `
+  // Insert data into the database
+  try {
+    await createMySQLPool.query(
+      `
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (?,?,?,?)
     `,
-    [customerId, amountInCents, status, date],
-  );
+      [customerId, amountInCents, status, date],
+    );
+  } catch (error) {
+    return {
+      message: `Error de Base de Datos: No se puede crear la factura. ${error}`,
+    };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const amount = Number(formData.get('amount'));
-  const customerId = formData.get('customerId');
-  const status = formData.get('status');
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
-  await createMySQLPool.query(
-    `
+  try {
+    await createMySQLPool.query(
+      `
           UPDATE invoices
           SET customer_id = ?, amount = ?, status = ?
           WHERE id = ?
       `,
-    [customerId, amountInCents, status, id],
-  );
+      [customerId, amountInCents, status, id],
+    );
+  } catch (error) {
+    return {
+      message: `Error de Base de Datos: No se puede actualizar la factura. ${error}`,
+    };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await createMySQLPool.query(`DELETE FROM invoices WHERE id = ?`, [id]);
-  revalidatePath('/dashboard/invoices');
+  try {
+    await createMySQLPool.query(`DELETE FROM invoices WHERE id = ?`, [id]);
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Factura eliminada.' };
+  } catch (error) {
+    return {
+      message: `Error de Base de Datos: No se puede eliminar la factura. ${error}`,
+    };
+  }
 }
